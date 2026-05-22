f42e07407bee8ddcd27aa90df5bfff4de7056eb9
wp-uruguay <manuel@wpuruguay.com>
2026-05-22 14:23:17 -0300

    fix: rango Unicode incorrecto en regex slugify

--- lib/utils.ts
+++ lib/utils.ts
@@ -36,7 +36,7 @@ export function slugify(text: string): string {
   return text
     .toLowerCase()
     .normalize("NFD")
-    .replace(/[Ì€-Í¯]/g, "")
+    .replace(/[\u0300-\u036f]/g, "")
     .replace(/[^\w\s-]/g, "")
     .replace(/\s+/g, "-")
     .replace(/-+/g, "-")
